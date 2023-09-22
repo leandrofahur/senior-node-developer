ffcca6d674950eaefa6bd196f1db3483f62b58c1
leandro <fahurleandro@gmail.com>
2023-09-22 16:24:29 -0700

    first spec test with vitest

---
+++ clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+
+test(`create an asnwer`, () => {
+  const answerQuestion = new AnswerQuestionUseCase();
+  const answer = answerQuestion.execute({
+    instructorId: "1",
+    questionId: "2",
+    content: "Answer content",
+  });
+
+  expect(answer.content).toEqual("Answer content");
+});
--- clean-ddd/src/domain/use-cases/answer-question.ts
+++ clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,5 +1,14 @@
+import { Answer } from "../entities/answer";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
 export class AnswerQuestionUseCase {
-  execute(): void {
-    // ...
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content);
+    return answer;
   }
 }
