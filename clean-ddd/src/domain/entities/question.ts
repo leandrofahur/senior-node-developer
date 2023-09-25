9383fa8a5fc03755a7df74df0128e5a6cc918a18
leandro <fahurleandro@gmail.com>
2023-09-25 09:39:41 -0700

    moving forward

--- clean-ddd/src/domain/entities/question.ts
+++ clean-ddd/src/domain/entities/question.ts
@@ -3,6 +3,7 @@ import { randomUUID } from "crypto";
 interface QuestionProps {
   title: string;
   content: string;
+  slug: string;
   authorId: string;
 }
 
@@ -10,11 +11,13 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: string;
   public authorId: string;
 
   constructor(props: Question, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
--- clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,17 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test(`create an answer`, () => {
-  const answerQuestion = new AnswerQuestionUseCase();
-  const answer = answerQuestion.execute({
+const answersRepositoryMock: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
+
+test(`create an answer`, async () => {
+  const answerQuestion = new AnswerQuestionUseCase(answersRepositoryMock);
+  const answer = await answerQuestion.execute({
     instructorId: "1",
     questionId: "2",
     content: "Answer content",
