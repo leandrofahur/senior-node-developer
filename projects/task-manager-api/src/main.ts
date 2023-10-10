72c44b441e16e05dbb2ca67181186ac58eeb89c3
leandro <fahurleandro@gmail.com>
2023-10-10 08:11:26 -0700

    succesfully refactored

--- projects/task-manager-api/src/main.ts
+++ projects/task-manager-api/src/main.ts
@@ -2,6 +2,8 @@ import app from "@infrastructure/api/server";
 
 const PORT = process.env.DOCKER_SERVER_PORT || 3000;
 
+// TODO: Add a try/catch block to handle errors and the db connection
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT} 🚀`);
 });
